ee9df2992a90be1cee6e9eca67e418ca35936205
Edris Sharif Rahmani <rahmani.biotech@gmail.com>
2023-02-06 22:17:40 +0330

    Add files via upload

--- restriction_site_finder.py
+++ restriction_site_finder.py
@@ -5,8 +5,11 @@ def restriction_site_finder(pattern, sequence):
     match = re.findall(pattern, sequence)
     site = re.finditer(pattern, sequence)
     p = [s.start() for s in site]
-    for i in range(0, len(match)):
-        print("Restriction site ",match[i], " found at ",p[i])
+    if len(match) == 0:
+        print("No match found.")
+    else:
+        for i in range(0, len(match)):
+            print("Restriction site for ",match[i], " found at ",p[i])
         
 EcoR1 = "GAATTC"
 sequence = "AGCTAGCTGCTAGCGAATTCTAGCTAGCTAGCTAAGCTAGCTCTAGAATTCGCTAGCTAGCTAGCTAGCTAGCT"
